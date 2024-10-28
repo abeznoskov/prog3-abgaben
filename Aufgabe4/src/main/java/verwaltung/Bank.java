d7ea53a0b3a729a7a5c68bc72cd212404c8d9ab0
abeznoskov <abeznoskov@gmail.com>
2024-10-28 10:52:18 +0100

    Implement methods

--- Aufgabe4/src/main/java/verwaltung/Bank.java
+++ Aufgabe4/src/main/java/verwaltung/Bank.java
@@ -1,55 +1,75 @@
 package verwaltung;
 
-import bankprojekt.verarbeitung.Geldbetrag;
-import bankprojekt.verarbeitung.Kunde;
+import bankprojekt.verarbeitung.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Bank {
-    public Bank(long bankleitzahl) {
 
+    private final long bankleitzahl;
+    private Map<Long, Konto> kontoListe;
+    private long vergebeneKontoNr = 0;
+    private final Geldbetrag  standardDispo = new Geldbetrag(200);
+
+    public Bank(long bankleitzahl) {
+        this.bankleitzahl = bankleitzahl;
     }
 
     public long getBankleitzahl() {
-        return 0;
+        return bankleitzahl;
     }
 
     public long girokontoErstellen(Kunde inhaber) {
-        return 0;
+        Girokonto k = new Girokonto(inhaber, vergebeneKontoNr +1, standardDispo);
+        kontoListe.put(vergebeneKontoNr, k);
+        return vergebeneKontoNr;
     }
 
     public long sparbuchErstellen(Kunde inhaber) {
-        return 0;
+        Sparbuch s = new Sparbuch(inhaber, vergebeneKontoNr +1);
+        kontoListe.put(vergebeneKontoNr, s);
+        return vergebeneKontoNr;
     }
 
     public String getAlleKonten() {
-        return "";
+        StringBuilder alleKonten = new StringBuilder();
+        for(Konto k: kontoListe.values())
+            alleKonten.append(k.toString()).append(" ");
+        return alleKonten.toString();
     }
 
     public List<Long> getAlleKontonummern() {
-        return null;
+        List<Long> alleKontonummern = new ArrayList<>();
+        for(Map.Entry<Long, Konto> k: kontoListe.entrySet())
+            alleKontonummern.add(k.getKey());
+        return alleKontonummern;
     }
 
-    public boolean geldAbheben(long von, Geldbetrag betrag) {
-        return false;
+    public boolean geldAbheben(long von, Geldbetrag betrag) throws GesperrtException {
+        return kontoListe.get(von).abheben(betrag);
     }
 
     public void geldEinzahlen(long auf, Geldbetrag betrag) {
-
+        kontoListe.get(auf).einzahlen(betrag);
     }
 
     public boolean kontoLoeschen(long nummer) {
-        return false;
+        kontoListe.remove(nummer);
+        return true;
     }
 
     public Geldbetrag getKontostand(long nummer) {
-        return null;
+        return kontoListe.get(nummer).getKontostand();
     }
 
     public boolean geldUeberweisen(long vonKontonr,
                                    long nachKontonr,
                                    Geldbetrag betrag,
-                                   String verwendungszweck) {
-        return false;
+                                   String verwendungszweck) throws GesperrtException {
+        kontoListe.get(vonKontonr).abheben(betrag);
+        kontoListe.get(nachKontonr).einzahlen(betrag);
+        return true;
     }
 }
