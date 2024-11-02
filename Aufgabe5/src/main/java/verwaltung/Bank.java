773022e977699b050f5431972515ff5f55ed5dfd
abeznoskov <abeznoskov@gmail.com>
2024-11-02 14:17:56 +0100

    Add Aufgabe5

---
+++ Aufgabe5/src/main/java/verwaltung/Bank.java
@@ -0,0 +1,206 @@
+package verwaltung;
+
+import bankprojekt.verarbeitung.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Bank fuer das Speichern und Verwalten von Konten
+ *
+ * @author Andreas Beznoskov,
+ *         Dinh Tuan Anh Nguyen
+ *
+ */
+public class Bank {
+
+    private final long bankleitzahl;
+    private Map<Long, Konto> kontoListe = new HashMap<>();
+    private long vergebeneKontoNr = 0;
+    private final Geldbetrag  standardDispo = new Geldbetrag(200);
+
+    /**
+     * Konstruktor mit Zuweisung der Bankleitzahl
+     *
+     * @param bankleitzahl die Bankleitzahl
+     *
+     */
+    public Bank(long bankleitzahl) {
+        this.bankleitzahl = bankleitzahl;
+    }
+
+    /**
+     * Getter-Methode der Bankleitzahl
+     *
+     * @return die Bankleitzahl
+     *
+     */
+    public long getBankleitzahl() {
+        return bankleitzahl;
+    }
+
+    /**
+     * Erstellt ein Girokonto für den angegebenen Kunden
+     *
+     * @param inhaber der Kunde
+     * @return die zugewiesene Kontonummer
+     * @throws NullPointerException wenn inhaber null
+     *
+     */
+    public long girokontoErstellen(Kunde inhaber) throws NullPointerException{
+        if (inhaber == null)
+            throw new NullPointerException();
+
+        this.vergebeneKontoNr += 1;
+
+        Girokonto k = new Girokonto(inhaber, vergebeneKontoNr, standardDispo);
+        this.kontoListe.put(vergebeneKontoNr, k);
+
+        return vergebeneKontoNr;
+    }
+
+    /**
+     * Erstellt ein Sparbuch für den angegebenen Kunden
+     *
+     * @param inhaber der Kunde
+     * @return die zugewiesene Kontonummer
+     * @throws NullPointerException wenn inhaber null
+     *
+     */
+    public long sparbuchErstellen(Kunde inhaber) throws NullPointerException {
+        if (inhaber == null)
+            throw new NullPointerException();
+
+        this.vergebeneKontoNr += 1;
+
+        Sparbuch s = new Sparbuch(inhaber, vergebeneKontoNr);
+        kontoListe.put(vergebeneKontoNr, s);
+
+        return vergebeneKontoNr;
+    }
+
+    /**
+     * Gibt alle Konten als ihr *.toString() zurück
+     *
+     * @return alle Konten als String
+     *
+     */
+    public String getAlleKonten() {
+        StringBuilder alleKonten = new StringBuilder();
+        for(Konto k: kontoListe.values()) {
+            alleKonten.append(k.toString());
+        }
+        return alleKonten.toString();
+    }
+
+    /**
+     * Gibt alle Kontonummern als List aus Long(s) zurück
+     *
+     * @return alle Kontonummern als List
+     *
+     */
+    public List<Long> getAlleKontonummern() {
+        List<Long> alleKontonummern = new ArrayList<>();
+        for(Map.Entry<Long, Konto> k: kontoListe.entrySet())
+            alleKontonummern.add(k.getKey());
+        return alleKontonummern;
+    }
+
+    /**
+     * Hebt Geld vom Konto mit der entpsprechender KontoNr ab
+     *
+     * @param von KontoNr
+     * @param betrag Betrag
+     * @return true  -> wenn Abheben erfolgreich
+     *         false -> wenn Abheben fehlgeschlagen
+     * @throws GesperrtException wenn Konto gesperrt
+     *
+     */
+    public boolean geldAbheben(long von, Geldbetrag betrag) throws GesperrtException {
+        Konto konto = kontoListe.get(von);
+
+        if (konto.isGesperrt())
+            throw new GesperrtException(konto.getKontonummer());
+
+        return konto.abheben(betrag);
+    }
+
+    /**
+     * Geldeinzahlung auf das Konto mit der entsprechenden KontoNr
+     *
+     * @param auf KontoNr
+     * @param betrag Betrag
+     * @throws GesperrtException wenn Konto gesperrt
+     *
+     */
+    public void geldEinzahlen(long auf, Geldbetrag betrag) throws GesperrtException {
+        Konto konto = kontoListe.get(auf);
+
+        if (konto.isGesperrt())
+            throw new GesperrtException(konto.getKontonummer());
+
+        konto.einzahlen(betrag);
+    }
+
+    /**
+     * Löscht das Konto mit der entsprechenden KontoNr
+     *
+     * @param nummer KontoNr
+     * @return true  -> wenn Löschen erfolgreich
+     *         false -> wenn Konto nicht gefunden
+     *
+     */
+    public boolean kontoLoeschen(long nummer) {
+        return kontoListe.remove(nummer) != null;
+    }
+
+    /**
+     * Gibt den Kontostand des Kontos mit der entsprechenden KontoNr aus
+     *
+     * @param nummer KontoNr
+     * @return Kontostand
+     *
+     */
+    public Geldbetrag getKontostand(long nummer) {
+        return kontoListe.get(nummer).getKontostand();
+    }
+
+    /**
+     * Überweisen von Geld (Bank-intern)
+     *
+     * @param vonKontonr KontoNr von der überwiesen wird
+     * @param nachKontonr KontoNr auf die überwiesen wird
+     * @param betrag der zu überweisende Betrag
+     * @param verwendungszweck Verwendunszweck
+     * @return true  -> wenn Überweisen erfolgreich
+     *         false -> wenn Überweisen fehlgeschlagen
+     * @throws GesperrtException wenn das Konto gesperrt
+     *
+     */
+    public boolean geldUeberweisen(long vonKontonr,
+                                   long nachKontonr,
+                                   Geldbetrag betrag,
+                                   String verwendungszweck) throws GesperrtException {
+
+        Konto von = kontoListe.get(vonKontonr);
+        Konto an = kontoListe.get(nachKontonr);
+
+        if (von == null | an == null)
+            return false;
+
+        if (von.isGesperrt())
+            throw new GesperrtException(von.getKontonummer());
+
+        if (an.isGesperrt())
+            throw new GesperrtException(an.getKontonummer());
+
+        if(!von.abheben(betrag))
+            return false;
+
+        an.einzahlen(betrag);
+
+        return true;
+    }
+}
---
+++ Aufgabe5/src/test/java/verwaltung/BankTest.java
@@ -0,0 +1,80 @@
+package verwaltung;
+
+import bankprojekt.verarbeitung.Geldbetrag;
+import bankprojekt.verarbeitung.GesperrtException;
+import bankprojekt.verarbeitung.Kunde;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO: Tests noch WIP
+class BankTest {
+    Bank bank; // Deklariere die Bank hier
+    static LocalDate geb = LocalDate.of(2020, 1, 1);
+    static Kunde testKundeA = new Kunde("David", "Schneider", "Arsch", geb);
+    static Kunde testKundeB = new Kunde("David2", "Schneider2", "Arsch2", geb);
+
+    @BeforeEach
+    void setUp() {
+        bank = new Bank(420); // Initialisiere die Bank in setUp
+    }
+
+    @Test
+    void getBankleitzahl() {
+        assertEquals(420L, bank.getBankleitzahl());
+    }
+
+    @Test
+    void girokontoUndSparbuchErstellen() {
+        assertEquals(1L, bank.girokontoErstellen(testKundeA)); // Erstelle das Girokonto
+        assertEquals(2L, bank.sparbuchErstellen(testKundeB)); // Erstelle das Sparbuch
+    }
+
+    @Test
+    void getAlleKonten() {
+        bank.girokontoErstellen(testKundeA);
+        bank.sparbuchErstellen(testKundeB);
+
+        //System.out.println(bank.getAlleKonten());
+    }
+
+    @Test
+    void getAlleKontonummern() {
+
+    }
+
+    @Test
+    void geldAbheben() {
+
+    }
+
+    @Test
+    void geldEinzahlen() {
+
+    }
+
+    @Test
+    void kontoLoeschen() {
+
+    }
+
+    @Test
+    void getKontostand() {
+
+    }
+
+    @Test
+    void geldUeberweisen() throws GesperrtException {
+        bank.girokontoErstellen(testKundeA);
+        bank.girokontoErstellen(testKundeB);
+        Geldbetrag g = new Geldbetrag(100);
+        bank.geldEinzahlen(1,g);
+
+        assertTrue(bank.geldUeberweisen(1,2, g, "Gegenstände die gegen das Kriegswaffenkontrollgesetz verstoßen"));
+        assertEquals(bank.getKontostand(1), new Geldbetrag(0));
+        assertEquals(bank.getKontostand(2), new Geldbetrag(100));
+    }
+}
