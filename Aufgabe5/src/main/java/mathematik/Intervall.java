be6cddaa817d6568ea6940ef730aed18882d62ae
abeznoskov <abeznoskov@gmail.com>
2024-11-05 22:28:25 +0100

    Update Intervall.java

--- Aufgabe5/src/main/java/mathematik/Intervall.java
+++ Aufgabe5/src/main/java/mathematik/Intervall.java
@@ -1,6 +1,6 @@
 package mathematik;
 
-public class Intervall<T> {
+public class Intervall<T extends Comparable<T>> {
     private T untereGrenze;
     private T obereGrenze;
 
@@ -18,15 +18,31 @@ public class Intervall<T> {
     }
 
     public boolean isLeer() {
-        return null != untereGrenze && null != obereGrenze;
+        return obereGrenze.compareTo(untereGrenze) < 0;
     }
 
     public <E> boolean enthaelt(E wert) {
-        return false;
+        if (wert == null || !wert.getClass().equals(untereGrenze.getClass())) {
+            return false; 
+        }
+        T wertT = (T) wert;
+
+
+        return (wertT.compareTo(untereGrenze) >= 0) && (wertT.compareTo(obereGrenze) <= 0);
     }
 
-    public <A> Intervall<T> schnitt(Intervall<A> anderes) {
-        return null;
+    public <A extends Comparable<A>> Intervall<T> schnitt(Intervall<A> anderes) {
+        if (anderes == null || anderes.untereGrenze.getClass() != this.untereGrenze.getClass()) {
+            return null;
+        }
+
+        T neueUntereGrenze = (this.untereGrenze.compareTo((T) anderes.untereGrenze) > 0) ? this.untereGrenze : (T) anderes.untereGrenze;
+        T neueObereGrenze = (this.obereGrenze.compareTo((T) anderes.obereGrenze) < 0) ? this.obereGrenze : (T) anderes.obereGrenze;
+
+        if (neueUntereGrenze.compareTo(neueObereGrenze) > 0) {
+            return new Intervall<>(null, null);
+        }
+
+        return new Intervall<>(neueUntereGrenze, neueObereGrenze);
     }
-    
 }
